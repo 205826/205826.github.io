11b51291535a5cce3060ebc90eaa74c85f65592d
T <141942251+205826@users.noreply.github.com>
2024-01-05 10:37:34 +0300

    Add files via upload

---
+++ T2P/T2P_scripts/000003.js
@@ -0,0 +1,31 @@
+//- name: async test
+//- description: Clock on T2P
+//- author: T
+//- semester: any
+//- input: inline_string
+//- input_default_value: 
+//- output: html
+//- import: math
+
+function getCurrentTime() {
+	var now = new Date();
+	var hours = now.getHours().toString().padStart(2, "0");
+	var minutes = now.getMinutes().toString().padStart(2, "0");
+	var seconds = now.getSeconds().toString().padStart(2, "0");
+
+	return hours + ":" + minutes + ":" + seconds;
+}
+
+function clock_step() {
+	// Update clock
+	output.all = '';
+  	output.print('Sandbox №'+input.sandbox_version+'<br>'); // Changes when removing libraries
+  	output.print('Build №'+input.build_version+'<br>'); // Change when editing program or input
+    output.print('Time: '+getCurrentTime()+'<br>');
+	output.flush(); // Upload from sandbox
+	
+	setTimeout(clock_step,100); // Minimum delay is now 100ms
+}
+clock_step();
+
+return 0;
--- T2P/T2P_scripts/199998.js
+++ T2P/T2P_scripts/199998.js
@@ -9,16 +9,9 @@
 //- input_default_value: 78 6
 //- input_default_value:   3
 //- output: html
-//- import: calc
+//- import: vivagraph
 
-function shuffleArray(array) {
-	for (var i = array.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-		var temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-}
+
 
 var poss = [];
 var out = '';
@@ -39,72 +32,81 @@ poss.map(p=>{
 	out+='<text x="'+p[0]*20+'" y="'+p[1]*20+'" dx="0" dy="20" fill="#FFF" font-size="10px">'+p[2]+'</text>';
 });
 out+='</svg><br><br>';
+if (Math.random()>0.5) output.print('Построим МСД для графа<br>');
+output.print(out);
 
 if (poss.length!=new Set(poss.map(x=>x[2])).size) {
-	output.print(out);
 	output.print_error('Ошибка ввода');
 	return 1;
 }
+var all_e = Array.from(new Set(poss.map(x=>x[2]))).sort((x,y)=>x-y);
+if (Math.random()>0.5) output.print('$$d_{ij} = |x_i-x_j|+|y_i-y_j|$$<br><br>');
+output.print('<table>');
+output.print('  <tr>');
+output.print('    <td></td>');
+all_e.map(x=>output.print('    <td>$$e_{'+(x)+'}$$</td>'));
+output.print('  </tr>');
+output.print('  <tr>');
 
-out+='$$d_{ij} = |x_i-x_j|+|y_i-y_j|$$<br><br>';
+var eg = [];
+
+all_e.map(x=>{
+	output.print('  <tr>');
+	output.print('    <td>$$e_{'+(x)+'}$$</td>');
+    all_e.map(y=>{
+      	var d = (a,b)=>Math.abs(a[0]-b[0])+Math.abs(a[1]-b[1]);
+        output.print('    <td>'+d(poss.filter(i=>i[2]==x)[0],poss.filter(i=>i[2]==y)[0])+'</td>');
+      	if (x<y)eg.push([d(poss.filter(i=>i[2]==x)[0],poss.filter(i=>i[2]==y)[0]),x,y]);
+    });
+	output.print('  <tr>');
+});
+
+output.print('</table><br>');
+output.print('Упорядочим ребра: ');
+eg = eg.sort((x,y)=>x[0]-y[0]);
+output.print(eg.map(x=>'$$(e_{'+x[1]+'}; e_{'+x[2]+'})$$').join('; ')+'<br><br>');
 
 var mkpair = [];
-var iter=0;
 var cost=0;
 var tree=new Array(16).fill(0).map((x,i)=>poss.map(y=>y[2]).indexOf(i)+1?i:0);
-while (iter++<100){
-	out+='$$d = '+iter+'$$:<br>';
-	const v=[];
-	poss.map(x=>poss.map(y=>x[2]<y[2]&&Math.abs(x[0]-y[0])+Math.abs(x[1]-y[1])==iter?v.push([x[2],y[2]]):0));
-	shuffleArray(v);
-	out+=v.map(x=>{
-		if(tree.filter(x=>x>0).sort()[0]==tree.filter(x=>x>0).sort().pop())
-			return '';
-		var a=x[0];
-		var b=x[1];
-		var n = '$$\\;\\;\\;\\;'+'(e_{'+x[0]+'}, e_{'+x[1]+'})'+'$$';
-		if (tree[a]==tree[b])
-			return n+': ЦИКЛ! Не добавляем';
-      	cost+=iter;
-		mkpair.push([a,b]);
-		var old_id = tree[b],  new_id = tree[a];
-		for (var j=0; j<16; j++)
-			if (tree[j] == old_id)
-				tree[j] = new_id;
-		return n+': Добавляем';
-	}).filter(x=>x).join('<br>');
-	
-	out+='<br>';
-	if(tree.filter(x=>x>0).sort()[0]==tree.filter(x=>x>0).sort().pop()){
-		out+='&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Все вершины соединены, дерево построено!';
-		break;
-	}
-	
-	out+='<br><br>';
-	//break;
-}
-out+='<br><br>';
-
-out+='<svg width="'+(max[0]*20+20)+'" height="'+(max[1]*20+40)+'" xmlns="http://www.w3.org/2000/svg">';
-for (var i=0;i<=max[0];i++){
-	out+='<line x1="'+(i*20+10)+'" y1="0" x2="'+(i*20+10)+'" y2="'+(max[1]*20+40)+'" stroke="#777" stroke-width="2" />';
-}
-for (var j=0;j<=max[1];j++){
-	out+='<line x1="0" y1="'+(j*20+23)+'" x2="'+(max[0]*20+20)+'" y2="'+(j*20+23)+'" stroke="#777" stroke-width="2" />';
-}
-var colors=['#F00','#FF0','#0F0','#0FF','#00F','#F0F','#F00','#FF0','#0F0','#0FF','#00F','#F0F','#F00','#FF0','#0F0','#0FF','#00F','#F0F'];
-mkpair.map((x,i)=>{
-	var p = x.map(y=> poss.filter(z=>z[2]==y));
-	var c=colors[Math.floor(Math.random()*colors.length)];
-	out+='<line x1="'+(p[0][0][0]*20+10)+'" y1="'+(p[0][0][1]*20+23)+'" x2="'+(p[1][0][0]*20+10)+'" y2="'+(p[0][0][1]*20+23)+'" stroke="'+c+'" stroke-width="'+((1-i/mkpair.length)*3+0.5)+'" />';
-	out+='<line x1="'+(p[1][0][0]*20+10)+'" y1="'+(p[0][0][1]*20+23)+'" x2="'+(p[1][0][0]*20+10)+'" y2="'+(p[1][0][1]*20+23)+'" stroke="'+c+'" stroke-width="'+((1-i/mkpair.length)*3+0.5)+'" />';
+var _outp = [];
+var texts = [];
+var rnd = [Math.random()>0.5, Math.random()>0.5];
+eg.map(x=>{
+  	if(tree.filter(x=>x>0).sort()[0]==tree.filter(x=>x>0).sort().pop()) return;
+    
+  	var a=x[1];
+    var b=x[2];
+    var n = '$$'+'(e_{'+a+'}, e_{'+b+'})'+'$$';
+    if (tree[a]==tree[b]){
+      _outp.push(JSON.parse(JSON.stringify(mkpair)));
+      	_outp[_outp.length-1].push([a,b,x[0]]);
+      if (rnd[0])
+     	return texts.push(n+': цикл - не добавляем<br>');
+      else
+     	return texts.push(n+' образует цикл с уже построенными ребрами<br>');
+    }
+    cost+=x[0];
+    mkpair.push([a,b,x[0]]);
+    var old_id = tree[b],  new_id = tree[a];
+    for (var j=0; j<16; j++)
+      if (tree[j] == old_id)
+        tree[j] = new_id;
+  _outp.push(JSON.parse(JSON.stringify(mkpair)));
+  if (rnd[1])
+    return texts.push(n+': Добавляем<br>');
+  else 
+    return texts.push('Включаем в дерево ребро '+n+':<br>');
+});
+//_outp.shift();
+vivagraph.renderer_svg(_outp,(s)=>{
+  output.print(s.map((x,i)=>texts[i]+x.replace(/width="200" height="200" viewBox=/,'width="400" height="400" viewBox=')).join('<br>')+'<br>');
+  if (Math.random()>0.5)
+    output.print('Суммарный вес ребер равен '+cost);
+  else 
+    output.print('Суммарный вес ребер МСД равен '+cost);
+  output.flush();
 });
 
-poss.map(p=>{
-	out+='<circle cx="'+(p[0]*20+10)+'" cy="'+(p[1]*20+23)+'" r="3" fill="#FFF"/>';
-	out+='<text x="'+p[0]*20+'" y="'+p[1]*20+'" dx="0" dy="20" fill="#FFF" font-size="10px">'+p[2]+'</text>';
-});
-out+='</svg><br>Cуммарный вес ребер: '+cost;
 
-output.print(out+'<br>');
 return 0;
