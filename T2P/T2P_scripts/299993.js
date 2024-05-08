0a3c30cfef8797b147690f870edd1ab52eccf7a8
T <141942251+205826@users.noreply.github.com>
2024-05-08 17:48:26 +0300

    Update 299993.js

--- T2P/T2P_scripts/299993.js
+++ T2P/T2P_scripts/299993.js
@@ -1,17 +1,19 @@
-//- name: Симпликс метод для канонической формы
+//- name: Симплекс метод для канонической формы
 //- description: метопы
 //- author: &#60;T&#62; + &#60;K&#62;
 //- semester: 4
 //- input: html
-//- input_default_value: $$c($$<input type="text" style="width:100px" id="T1c" value="0.1 0.255"><br>
-//- input_default_value: $$b($$<input type="text" style="width:100px" id="T1b" value="3 27 22"><br>
-//- input_default_value: $$a\Bigg($$<textarea id="T1a" style="width:110px;height:55px;vertical-align:middle;">0 1 -1 0 0\n3 2.17 0 -1 0\n1 2.67 0 0 -1</textarea>$$\Bigg)$$<br>
+//- input_default_value: $$c($$<input type="text" style="width:100px" id="T1c" value="-0.5 0 0 -1 0"><br>
+//- input_default_value: $$b($$<input type="text" style="width:100px" id="T1b" value="-1 -1 3"><br>
+//- input_default_value: $$a\Bigg($$<textarea id="T1a" style="width:110px;height:55px;vertical-align:middle;">12 6 0 -18 0\n12 0 6 -18 0\n3 0 0 -6 3</textarea>$$\Bigg)$$<br>
 //- input_default_value: Поиск <select id="T1m"><option>минимума</option><option selected>максимума</option></select><br>
 //- input_default_value: Отображение формул: <select id="T1tex"><option>Mathjs</option><option>Tex</option></select><br>
+//- input_default_value: Алгоритм: <select id="T1alg"><option>Симплекс метод для канонической формы</option><option>Двойственный симплекс метод для канонической формы</option></select><br>
 //- output: html
 //- import: math
 //- import: nerdamer
 //try{(()=>{
+
 var _ = (x) => {
 	let s = math.evaluate(x).toString();
 	if (s === 'true') return true;
@@ -22,6 +24,7 @@ const _2 = (s,i)=>math.round(s,i||5);
 var _toTex = (values,labels_f)=>values.map((x,i)=>_(`${x}!=0`)?(_(`${x}<0`)?'-':'+')+(Math.abs(x)!=1?_2(Math.abs(x)):'')+''+(Math.abs(x)==1?labels_f(i+1)||1:labels_f(i+1)):'').join('').replace(/^\+/,'');
 var parseMatrix = (a)=>a.split('\n').filter(x=>x.trim()).map(x=>x.split(' ').filter(y=>y.trim()).map(x=>parseFloat(x)));
 
+const is_dual = input.by_id('T1alg').value!='Симплекс метод для канонической формы';
 const find_min = input.by_id('T1m').value=='минимума';
 const C = parseMatrix(input.by_id('T1c').value)[0];
 const B = parseMatrix(input.by_id('T1b').value)[0];
@@ -52,6 +55,7 @@ output.print('$$\\left\\{\\begin{align}'+
 	' \\\\ '+
 	'x_i\\geq 0,i = 1,'+A[0].length+
 '\\end{align}\\right.$$<br/>');
+if (is_dual){return dual();}
 output.print('Применим метод искусственного базиса. Для этого введем переменные $$'+A.map((x,i)=>'y_'+(i+1)).join(',')+'$$<br/>');
 
 const tmp_A = A.map((x,i)=>x.concat(A.map((x,j)=>i==j?1:0)));
@@ -363,5 +367,172 @@ if (input.by_id('T1tex').value=='Tex'){output.all=output.all.replace(/\$\$\$\$/g
 /*})();}catch(e){
 	output.all='!!!!!'+e+'<br>'+output.all;
 }*/
+function dual(){
+	output.print('Двойственная задача имеет вид:<br>');
+	const del_ind = C.map((x,i)=>x?-1:i);
+	const dual_B=C.filter(x=>x);
+	const dual_C=B.concat(new Array(dual_B.length).fill(0));
+	const dual_A=math.transpose(A.map(x=>x.filter((x,i)=>del_ind.indexOf(i)==-1))).map(x=>x.concat(new Array(dual_B.length).fill(0))).map((x,i)=>{x[A.length+i]=find_min?1:-1;return x;});
+	output.print((find_min?'max':'min')+'{$$'+_toTex(dual_C,(i)=>'y_'+i)+'$$} при ограничениях:<br>');
+	
+	output.print('$$\\left\\{\\begin{align}'+
+	dual_A.map((x,i)=>_toTex(x,(i)=>'y_'+i)+'&='+dual_B[i]).join(' \\\\')+
+		' \\\\ '+
+		'y_i\\geq 0,i = 1,'+dual_A[0].length+
+	'\\end{align}\\right.$$<br/>');
+	
+	let lables = [[],dual_A[0].map((x,i)=>'y_'+(i+1))];
+	dual_B.map(x=>lables[0].unshift(lables[1].pop()));
+	let matrix = {};
+	dual_A.map((x,i)=>{
+		matrix[lables[0][i]]={};
+		x.map((y,j)=>{
+			if (lables[1][j]) matrix[lables[0][i]][lables[1][j]]=y;
+		});
+		matrix[lables[0][i]].b=dual_B[i];
+	});
+	
+	
+	lables[0].push('f');
+	lables[1].push('b');
+	let f = {};
+	dual_C.map((x,i)=>f['y_'+(i+1)]=x);
+	
+	function dual_check_is_answer_ready_max(w_row) {
+	//output.print(w_row);
+		return Object.keys(w_row).filter(x=>x!='b').every((k, i) => _(`${w_row[k]} >= 0`));
+	}
+	function dual_select_next_column_max() {
+		let res = "0";
+		let res_key = "";
+		for (let key of Object.keys(f)) {
+			if (key === "b") continue;
 
+			if (_(`${f[key]} < 0`) && _(`${f[key]} < ${res}`)) {
+				res = f[key];
+				res_key = key;
+			}
+		}
+		if (res_key === "") throw "не смогли выбрать столбец";
+		return res_key;
+	}
+
+	// среди всех строк выбранной колонки, вибирает такую строку,
+	// при увеличении которой, b в этой строке будет быстрее отальных b стремиться к 0
+	//
+	// к примеру column_var мб "x_3"
+	function dual_select_next_row(column_var) {
+		let keys = Object.keys(matrix);
+		let res_val = Number.MAX_VALUE;
+		let res_key = "";
+		for (let row_index = 0; row_index < keys.length; row_index++) {
+			let row_key = keys[row_index];
+			let row = matrix[row_key];
+			let b = row.b || 0;
+			let v = row[column_var] || 0;
+			if (v==0) continue;
+			let approx_b_val = _(`(${b})/(${v})`);
+			if (_(`${approx_b_val} < ${res_val}`) && _(`${approx_b_val} > 0`)) {
+				res_key = row_key;
+				res_val = approx_b_val;
+			}
+		}
+		if (res_key === "") throw "не смогли выбрать строку у столбца " + column_var;
+		return res_key;
+	}
+
+	// 2 x + 4 y + 3 -> obj
+	function dual_extract_coffs(str) {
+		let obj = {};
+		Array.from(str.matchAll(/[xy]_\d+/g)).map(x=>obj[x] = math.derivative(str.replace(/_/g,''), x[0].replace(/_/g,'')).evaluate());
+		str=str.replace(/[xy]_\d+/g,'0');
+		obj.b = math.evaluate(str);
+		return obj;
+	}
+
+	// возвращает без "="
+	function dual_stringify_row(sel_row) {
+		return Object.keys(matrix[sel_row])
+			.map(key => key === "b" ? matrix[sel_row].b
+				: "" + key + "*" + matrix[sel_row][key])
+			.join("+");
+	}
+	let lables_swap = (a,b) => {
+		var ai=lables[0].indexOf(a);
+		var bi=lables[1].indexOf(b);
+		var tmp = lables[0][ai];
+		lables[0][ai] = lables[1][bi];
+		lables[1][bi] = tmp;
+	};
+		
+	let max_iter=20;
+	while (!(dual_check_is_answer_ready_max(f) || !(max_iter--))) {
+		//output.print(f);
+		// выбираем следующик колонку и строку
+		let sel_column = dual_select_next_column_max();
+		//output.print(matrix);
+		let sel_row = dual_select_next_row(sel_column);
+
+		matrix.f = f;
+		print_matrix(lables[0], lables[1],matrix, sel_column, sel_row);
+		//output.print(matrix);
+		delete matrix.f;
+		output.print('Выбираем большую по модулю положительную $$\\Delta$$. Видим, что при увеличении $$'+sel_column+'$$ быстрее всего до нуля доходит $$'+sel_row+'$$.<br>');
+		output.print('Меняем $$'+sel_row+'$$ и $$'+sel_column+'$$ местами.<br>');
+
+		// выражаем переменную, которая была в sel_column из строки sel_row
+		let eq_str = dual_stringify_row(sel_row);
+		eq_str += "=" + sel_row;
+
+		let solvedStr = nerdamer(eq_str).solveFor(sel_column).toString();
+		solvedStr = math.rationalize(math.simplify(math.rationalize(solvedStr))).toString();
+		let row_obj = extract_coffs(solvedStr);
+
+		// удалим старую
+		delete matrix[sel_row];
+		let obj = {};
+		matrix.f = f;
+		lables[0].map(_=>{
+			if (_==sel_row)return;
+			let tex = _toTex(lables[1].map(x=>matrix[_][x]||0),i=>lables[1][i-1]=='b'?'':lables[1][i-1]);
+			obj[_]=tex;
+		});
+		//output.print(obj);
+		lables_swap(sel_row,sel_column);
+		// изменим остальные строки
+		for(let key of Object.keys(matrix)){
+			//let row = matrix[key]
+			let row_str = dual_stringify_row(key);
+			row_str = row_str.replaceAll(sel_column, `(${solvedStr})`);
+			let simpl = math.rationalize(math.simplify(math.rationalize(row_str))).toString();
+			simpl = simpl.replace(/\d(\.\d+)?e-\d+/g,'0');
+			matrix[key] = dual_extract_coffs(simpl);
+		}
+		f=matrix.f;
+		delete matrix.f;
+
+		// а теперь добавим новую
+		matrix[sel_column] = row_obj;
+		let column_tex = '';
+		[0,1].map(_3=>{
+			lables[0].map(_=>{
+				if (_=='f')return;
+
+				let tex = _toTex(lables[1].map(x=>matrix[_][x]||0),i=>lables[1][i-1]=='b'?'':lables[1][i-1]);
+				if (_3^+(_==sel_column)) output.print('$$'+_+' = '+(obj[_]?obj[_].replace(sel_column,'('+column_tex+')')+' = $$$$':'')+tex+'$$<br>');
+				if (!_3&&_==sel_column) column_tex=tex;
+			});
+		});
+		output.print('$$F = '+obj.f.replace(sel_column,'('+column_tex+')')+' = $$$$'+_toTex(lables[1].map(x=>f[x]||0),i=>lables[1][i-1]=='b'?'':lables[1][i-1])+'$$<br>');
+		output.print('<br>');
+	}
+	matrix.f = f;
+	print_matrix(lables[0], lables[1],matrix, '', '');
+	output.print('Обе характеристические разности отрицательные. Найдено оптимальное решение.<br>');
+
+	output.print('$$f^* = '+_2((find_min?1:-1)*(matrix.f.b||0))+'$$<br>');
+
+	if (input.by_id('T1tex').value=='Tex'){output.all=output.all.replace(/\$\$\$\$/g,'').replace(/\$/g,'<span>$</span>');}
+	return 0;
+}
 return 0;
